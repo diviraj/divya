b283ef81613867f6be1a3477bbb6ba71c736d9d4
DIVYA S <divyasundarraj1996@gmail.com>
2016-06-09 22:11:54 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main() 
+{
+   char str[100], temp;
+   int i, j = 0;
+   gets(str);
+   i = 0;
+   j = strlen(str) - 1;
+	while (i < j)
+	{
+      	temp = str[i];
+      	str[i] = str[j];
+      	str[j] = temp;
+      i++;
+      j--;
+   }
+printf("Reverse string is :%s", str);
+   return (0);
+}
