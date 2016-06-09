2bfa4b53c1d616118e56aecf45e6bb0d2f37d856
DIVYA S <divyasundarraj1996@gmail.com>
2016-06-09 22:15:42 +0530

    Create factnum.c

---
+++ factnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int i=1,f=1,num;
+  scanf("%d",&num);
+  while(i<=num){
+      f=f*i;
+      i++;
+  }
+
+  printf("Factorial of %d is: %d",num,f);
+  return 0;
+}
